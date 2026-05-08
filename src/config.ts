924389171d1e1149aba72819e9fd48df8a94eda4
hbxlkl <xproxy@qq.com>
2026-05-08 18:34:46 +0800

    修改了网站标题
    修改了网站标题

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
+	title: "石门摄影",
+	subtitle: "摄影作品分享和展示",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
