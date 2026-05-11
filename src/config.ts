b32ac3a52f7cfddb830e3216b57185b2240acc56
hbxlkl <xproxy@qq.com>
2026-05-11 11:25:00 +0800

    设置显示横幅图片 Enable banner feature in config
    Changed banner enable setting to true to activate the banner feature.

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true, // 重点：必须设为true，设为false就是关闭横幅功能，图片肯定不显示
 		src: "assets/images/hengfu.jpeg", // 横幅图片地址 Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
