10a53d52d65093f2cbfda6cd966638ff8f88816a
hbxlkl <xproxy@qq.com>
2026-05-11 15:00:21 +0800

    Remove redundant line for About link in navBarConfig

--- src/config.ts
+++ src/config.ts
@@ -50,10 +50,7 @@ export const navBarConfig: NavBarConfig = {
 	links: [
 		LinkPreset.Home, // 主页 自动中文
 		LinkPreset.Archive, // 归档 自动中文
-		LinkPreset.About, // 关于 自动中文
-
-
-		
+		LinkPreset.About, // 关于 自动中文	
 		{			
 			name: "GitHub仓库", // 链接名称
 			url: "https://github.com/hbxlkl/fublog", // 链接网址
