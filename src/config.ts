3a80803e93621af2578debf69dae36df2dea97da
hbxlkl <xproxy@qq.com>
2026-05-11 16:17:35 +0800

    添加 '友链' 链接代码link to navBarConfig

--- src/config.ts
+++ src/config.ts
@@ -50,7 +50,8 @@ export const navBarConfig: NavBarConfig = {
 	links: [
 		LinkPreset.Home, // 主页 自动中文
 		LinkPreset.Archive, // 归档 自动中文
-		LinkPreset.About, // 关于 自动中文	
+		LinkPreset.About, // 关于 自动中文
+		LinkPreset.友链, // 关于 自动中文
 		{			
 			name: "GitHub仓库", // 链接名称
 			url: "https://github.com/hbxlkl/fublog", // 链接网址
