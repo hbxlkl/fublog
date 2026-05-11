2aede6b182b236d718d321dc2b320add44261078
hbxlkl <xproxy@qq.com>
2026-05-11 16:19:51 +0800

    删除友链 from links array

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home, // 主页 自动中文
 		LinkPreset.Archive, // 归档 自动中文
 		LinkPreset.About, // 关于 自动中文
-		LinkPreset.友链, // 关于 自动中文
+
 		{			
 			name: "GitHub仓库", // 链接名称
 			url: "https://github.com/hbxlkl/fublog", // 链接网址
